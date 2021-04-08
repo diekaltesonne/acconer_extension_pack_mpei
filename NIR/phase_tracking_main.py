f5c70357c1c20875e888264c63f9bf65d5f0f331
diekaltesonne <diekaltesonne2000@mail.ru>
2021-04-08 09:20:43 +0300

    delete not needed info

--- NIR/phase_tracking_main.py
+++ NIR/phase_tracking_main.py
@@ -12,52 +12,7 @@ from acconeer_utils.pg_process import PGProcess, PGProccessDiedException
 #создание загрузочного  файла(текстовый ) с отчетами IQ
 
 
-def main():
-    args = example_utils.ExampleArgumentParser(num_sens=1).parse_args()
-    example_utils.config_logging(args)
 
-    #interface choose
-    if args.socket_addr:
-        client = SocketClient(args.socket_addr)# получение сырого потока данных с интерфейса
-    elif args.spi:
-        client = SPIClient()# получение сырого потока данных с интерфейса
-    else:
-        port = args.serial_port or example_utils.autodetect_serial_port()
-        client = UARTClient(port)# получение сырого потока данных с интерфейса
-
-    sensor_config = get_sensor_config()
-    sensor_config.sensor = args.sensors
-
-    processing_config = get_processing_config()
-
-    session_info = client.setup_session(sensor_config)
-
-    pg_updater = PGUpdater(sensor_config, processing_config, session_info)
-    pg_process = PGProcess(pg_updater)
-
-    pg_process.start()
-    client.start_streaming()
-
-    interrupt_handler = example_utils.ExampleInterruptHandler()
-    print("Press Ctrl-C to end session")
-
-    processor = PhaseTrackingProcessor(sensor_config, processing_config, session_info)
-
-    #передача от сырого потока в обработчик
-    while not interrupt_handler.got_signal:
-        info, sweep = client.get_next()
-        plot_data = processor.process(sweep)
-        logger(plot_data)
-        if plot_data is not None:
-            try:
-                pg_process.put_data(plot_data)
-            except PGProccessDiedException:
-                break
-    #передача от сырого потока в обработчик
-
-    print("Disconnecting...")
-    pg_process.close()
-    client.disconnect()
 
 def logger(data):
     with open('text.txt','w') as f:
@@ -73,6 +28,79 @@ def get_sensor_config():
 def get_processing_config():
     return {}
 
+class PGUpdater:
+    def __init__(self, sensor_config, processing_config, session_info):
+        self.config = sensor_config
+        self.interval = sensor_config.range_interval
+
+    def setup(self, win):
+        win.resize(800, 600)
+        win.setWindowTitle("Acconeer phase tracking example")
+
+        self.abs_plot = win.addPlot(row=0, col=0)
+        self.abs_plot.showGrid(x=True, y=True)
+        self.abs_plot.setLabel("left", "Amplitude")
+        self.abs_plot.setLabel("bottom", "Depth (m)")
+        self.abs_curve = self.abs_plot.plot(pen=example_utils.pg_pen_cycler(0))
+        pen = example_utils.pg_pen_cycler(1)
+        pen.setStyle(QtCore.Qt.DashLine)
+        self.abs_inf_line = pg.InfiniteLine(pen=pen)
+        self.abs_plot.addItem(self.abs_inf_line)
+
+        self.arg_plot = win.addPlot(row=1, col=0)
+        self.arg_plot.showGrid(x=True, y=True)
+        self.arg_plot.setLabel("bottom", "Depth (m)")
+        self.arg_plot.setLabel("left", "Phase")
+        self.arg_plot.setYRange(-np.pi, np.pi)
+        self.arg_plot.getAxis("left").setTicks(example_utils.pg_phase_ticks)
+        self.arg_curve = self.arg_plot.plot(pen=example_utils.pg_pen_cycler(0))
+        self.arg_inf_line = pg.InfiniteLine(pen=pen)
+        self.arg_plot.addItem(self.arg_inf_line)
+
+        self.iq_plot = win.addPlot(row=1, col=1, title="IQ at line")
+        example_utils.pg_setup_polar_plot(self.iq_plot, 0.5)
+        self.iq_curve = self.iq_plot.plot(pen=example_utils.pg_pen_cycler())
+        self.iq_scatter = pg.ScatterPlotItem(
+                brush=pg.mkBrush(example_utils.color_cycler()),
+                size=15,
+                )
+        self.iq_plot.addItem(self.iq_scatter)
+
+        self.hist_plot = win.addPlot(row=0, col=1, colspan=2)
+        self.hist_plot.showGrid(x=True, y=True)
+        self.hist_plot.setLabel("bottom", "Time (s)")
+        self.hist_plot.setLabel("left", "Tracking (mm)")
+        self.hist_curve = self.hist_plot.plot(pen=example_utils.pg_pen_cycler())
+        self.hist_plot.setYRange(-5, 5)
+
+        self.hist_zoom_plot = win.addPlot(row=1, col=2)
+        self.hist_zoom_plot.showGrid(x=True, y=True)
+        self.hist_zoom_plot.setLabel("bottom", "Time (s)")
+        self.hist_zoom_plot.setLabel("left", "Tracking (mm)")
+        self.hist_zoom_curve = self.hist_zoom_plot.plot(pen=example_utils.pg_pen_cycler())
+        self.hist_zoom_plot.setYRange(-0.5, 0.5)
+
+        self.smooth_max = example_utils.SmoothMax(self.config.sweep_rate)
+        self.first = True
+
+    def update(self, data):
+        if self.first:
+            self.xs = np.linspace(*self.interval, len(data["abs"]))
+            self.ts = np.linspace(-3, 0, len(data["hist_pos"]))
+            self.ts_zoom = np.linspace(-1.5, 0, len(data["hist_pos_zoom"]))
+            self.first = False
+
+        com_x = (1-data["com"])*self.interval[0] + data["com"]*self.interval[1]
+
+        self.abs_curve.setData(self.xs, data["abs"])
+        self.abs_plot.setYRange(0, self.smooth_max.update(np.amax(data["abs"])))
+        self.abs_inf_line.setValue(com_x)
+        self.arg_curve.setData(self.xs, data["arg"])
+        self.arg_inf_line.setValue(com_x)
+        self.hist_curve.setData(self.ts, data["hist_pos"])
+        self.hist_zoom_curve.setData(self.ts_zoom, data["hist_pos_zoom"])
+        self.iq_curve.setData([0, np.real(data["iq_val"])], [0, np.imag(data["iq_val"])])
+        self.iq_scatter.setData([np.real(data["iq_val"])], [np.imag(data["iq_val"])])
 
 class PhaseTrackingProcessor:
 
@@ -176,82 +204,53 @@ class PhaseTrackingProcessor:
     def alpha(self, tau, dt):
         return 1 - np.exp(-dt/tau)
 
+def main():
+    args = example_utils.ExampleArgumentParser(num_sens=1).parse_args()
+    example_utils.config_logging(args)
 
+    #interface choose
+    if args.socket_addr:
+        client = SocketClient(args.socket_addr)# получение сырого потока данных с интерфейса
+    elif args.spi:
+        client = SPIClient()# получение сырого потока данных с интерфейса
+    else:
+        port = args.serial_port or example_utils.autodetect_serial_port()
+        client = UARTClient(port)# получение сырого потока данных с интерфейса
 
+    sensor_config = get_sensor_config()
+    sensor_config.sensor = args.sensors
 
-class PGUpdater:
-    def __init__(self, sensor_config, processing_config, session_info):
-        self.config = sensor_config
-        self.interval = sensor_config.range_interval
+    processing_config = get_processing_config()
 
-    def setup(self, win):
-        win.resize(800, 600)
-        win.setWindowTitle("Acconeer phase tracking example")
+    session_info = client.setup_session(sensor_config)
 
-        self.abs_plot = win.addPlot(row=0, col=0)
-        self.abs_plot.showGrid(x=True, y=True)
-        self.abs_plot.setLabel("left", "Amplitude")
-        self.abs_plot.setLabel("bottom", "Depth (m)")
-        self.abs_curve = self.abs_plot.plot(pen=example_utils.pg_pen_cycler(0))
-        pen = example_utils.pg_pen_cycler(1)
-        pen.setStyle(QtCore.Qt.DashLine)
-        self.abs_inf_line = pg.InfiniteLine(pen=pen)
-        self.abs_plot.addItem(self.abs_inf_line)
+    pg_updater = PGUpdater(sensor_config, processing_config, session_info)
+    pg_process = PGProcess(pg_updater)
 
-        self.arg_plot = win.addPlot(row=1, col=0)
-        self.arg_plot.showGrid(x=True, y=True)
-        self.arg_plot.setLabel("bottom", "Depth (m)")
-        self.arg_plot.setLabel("left", "Phase")
-        self.arg_plot.setYRange(-np.pi, np.pi)
-        self.arg_plot.getAxis("left").setTicks(example_utils.pg_phase_ticks)
-        self.arg_curve = self.arg_plot.plot(pen=example_utils.pg_pen_cycler(0))
-        self.arg_inf_line = pg.InfiniteLine(pen=pen)
-        self.arg_plot.addItem(self.arg_inf_line)
+    pg_process.start()
+    client.start_streaming()
 
-        self.iq_plot = win.addPlot(row=1, col=1, title="IQ at line")
-        example_utils.pg_setup_polar_plot(self.iq_plot, 0.5)
-        self.iq_curve = self.iq_plot.plot(pen=example_utils.pg_pen_cycler())
-        self.iq_scatter = pg.ScatterPlotItem(
-                brush=pg.mkBrush(example_utils.color_cycler()),
-                size=15,
-                )
-        self.iq_plot.addItem(self.iq_scatter)
+    interrupt_handler = example_utils.ExampleInterruptHandler()
+    print("Press Ctrl-C to end session")
 
-        self.hist_plot = win.addPlot(row=0, col=1, colspan=2)
-        self.hist_plot.showGrid(x=True, y=True)
-        self.hist_plot.setLabel("bottom", "Time (s)")
-        self.hist_plot.setLabel("left", "Tracking (mm)")
-        self.hist_curve = self.hist_plot.plot(pen=example_utils.pg_pen_cycler())
-        self.hist_plot.setYRange(-5, 5)
+    processor = PhaseTrackingProcessor(sensor_config, processing_config, session_info)
 
-        self.hist_zoom_plot = win.addPlot(row=1, col=2)
-        self.hist_zoom_plot.showGrid(x=True, y=True)
-        self.hist_zoom_plot.setLabel("bottom", "Time (s)")
-        self.hist_zoom_plot.setLabel("left", "Tracking (mm)")
-        self.hist_zoom_curve = self.hist_zoom_plot.plot(pen=example_utils.pg_pen_cycler())
-        self.hist_zoom_plot.setYRange(-0.5, 0.5)
+    #передача от сырого потока в обработчик
+    while not interrupt_handler.got_signal:
+        info, sweep = client.get_next()
+        plot_data = processor.process(sweep)
+        if plot_data is not None:
+            try:
+                pg_process.put_data(plot_data)
+                logger(str(plot_data))
+            except PGProccessDiedException:
+                break
+    #передача от сырого потока в обработчик
 
-        self.smooth_max = example_utils.SmoothMax(self.config.sweep_rate)
-        self.first = True
+    print("Disconnecting...")
+    pg_process.close()
+    client.disconnect()
 
-    def update(self, data):
-        if self.first:
-            self.xs = np.linspace(*self.interval, len(data["abs"]))
-            self.ts = np.linspace(-3, 0, len(data["hist_pos"]))
-            self.ts_zoom = np.linspace(-1.5, 0, len(data["hist_pos_zoom"]))
-            self.first = False
-
-        com_x = (1-data["com"])*self.interval[0] + data["com"]*self.interval[1]
-
-        self.abs_curve.setData(self.xs, data["abs"])
-        self.abs_plot.setYRange(0, self.smooth_max.update(np.amax(data["abs"])))
-        self.abs_inf_line.setValue(com_x)
-        self.arg_curve.setData(self.xs, data["arg"])
-        self.arg_inf_line.setValue(com_x)
-        self.hist_curve.setData(self.ts, data["hist_pos"])
-        self.hist_zoom_curve.setData(self.ts_zoom, data["hist_pos_zoom"])
-        self.iq_curve.setData([0, np.real(data["iq_val"])], [0, np.imag(data["iq_val"])])
-        self.iq_scatter.setData([np.real(data["iq_val"])], [np.imag(data["iq_val"])])
 
 if __name__ == "__main__":
     main()
